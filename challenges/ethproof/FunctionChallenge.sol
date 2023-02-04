a7da73379d3227d3dd7c9b7e7180cf80407b28f0
Mewtoo <60632424+Memewtoo@users.noreply.github.com>
2023-02-04 13:21:40 +0800

    Solution for Function Challenge in Lesson 2

---
+++ challenges/ethproof/FunctionChallenge.sol
@@ -0,0 +1,29 @@
+/ SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
+pragma solidity ^0.8.17;
+
+contract FunctionChallenge {
+    uint public minContribution;
+
+    /** Instatantiate the minimum contribution upon creating an instance */
+    constructor(uint num){
+        minContribution = num;
+    }
+
+    /** Allow to send money to the contract */
+    function contribute() public payable {
+
+    }
+
+    /** Displays the minimum contribution*/
+    function getMinContribution() public view returns(uint){
+        return minContribution;
+    }
+
+    /** Gets the sum of two numbers*/
+    function getSum(uint num1, uint num2)public pure returns(uint){
+        uint sum = num1 + num2;
+        return sum;
+    }
+    
+}
