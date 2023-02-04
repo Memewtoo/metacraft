f9e387cbb77e2101fa60d96ff46943535182b220
Mewtoo <60632424+Memewtoo@users.noreply.github.com>
2023-02-04 22:50:16 +0800

    Final challenge from Metacrafters course

---
+++ challenges/ethproof/Project.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
+pragma solidity ^0.8.17;
+
+contract Project {
+    uint256 minContribution;
+
+    /** Constructor function that is automatically called
+        when an instance is created 
+    */
+    constructor (uint256 minimum) public {
+        minContribution = minimum;
+    }
+
+    /** Different types error handling */
+    function contribute() public payable {
+        require(msg.value >= minContribution, "Insufficient amount to be able to contribute");
+    }
+
+    function getContribute() public payable {
+        assert(msg.value >= minContribution)
+    }
+
+    error InvalidAmount (uint256 sent, uint256 minRequired);
+
+    function validContribute() public payable {
+        uint256 amount = msg.value;
+
+        if(amount < minContribution){
+            revert invalidAmount({
+                sent: amount,
+                minRequired: minContribution
+            });
+        }
+    }
+}
