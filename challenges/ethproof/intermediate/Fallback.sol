65530311fc39354a665b42aa0fde540e99d41dc6
“Memewtoo” <myuutuh@gmail.com>
2023-02-08 21:55:24 +0800

    Fallback Challenge solution

---
+++ challenges/ethproof/intermediate/Fallback.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+contract Fallback{
+
+    /** Fallback function*/
+    fallback() external payable {
+
+    }
+}
+
+contract Sender{
+    //Tries to invoke non existent function
+    function transfer(Fallback fall) public payable returns(bool) {
+        (bool success,) = address(fall).call(abi.encodeWithSignature("nonExistingFunction()"));
+        require (success);
+
+        return true;
+    }
+}
