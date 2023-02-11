f1ae64aadaee372e536b15293f81a6f7bf97f619
“Memewtoo” <myuutuh@gmail.com>
2023-02-11 08:50:27 +0800

    Fund token updated

--- challenges/ethproof/intermediate/TokenProject/FundTokenProject/scripts/deploy.js
+++ challenges/ethproof/intermediate/TokenProject/FundTokenProject/scripts/deploy.js
@@ -3,8 +3,8 @@ const hre = require("hardhat");
 async function main() {
   const FundToken = await hre.ethers.getContractFactory("FundToken");
 
-  /** Deploys FundToken contract with 10,000,000 token cap and 5 block reward */
-  const fundToken = await FundToken.deploy(10000000, 5);
+  /** Deploys FundToken contract with 10,000,000 token */
+  const fundToken = await FundToken.deploy(10000000);
 
   await fundToken.deployed();
 
